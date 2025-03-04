f6fb2892bfde733ac3661da343b4b4b149dd7a24
NotBrian122 <s00254527@atu.ie>
2025-03-04 15:55:12 +0000

    fixed the app compontent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, booleanAttribute } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IOMDBResponse } from './omdbresponse';
+import { OmdbApiService } from './services/omdb-api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Week7API';
-}
+  title = 'Movie Finder';
+  movieData?:IOMDBResponse|undefined;
+  errorMessage:any;
+
+  constructor(private _omdbService:OmdbApiService){}
+
+    getMoveIeDetails(movieName:string): boolean {
+      this._omdbService.getMovieData(movieName).subscribe(
+        movieData => {
+          this.movieData=movieData;
+          console.log("Drirector Name: "+ this.movieData.Director);
+        }
+      )
+      return false;
+    }
+  }
+  
+
